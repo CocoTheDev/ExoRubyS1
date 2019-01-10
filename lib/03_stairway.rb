e64a106752e68c5f85c8a0dada73512fab891df4
Maxime Smolis <mpsmolis@gmail.com>
2019-01-10 21:30:37 +0100

    exo jeu terminé

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,39 +1,35 @@
 
-# ----- POUR UNE PARTIE ----- 
-# def partie  
+def partie  
 
-#     x = 0 
-#     nb_lances = 0
-#     until x == 10
-#         puts "Le lancé de dé vous donne"
-#         nb = rand(1..6).to_i
-#         nb_lances += 1
-#         if nb == 1
-#             x -= 1
-#             if x < 0
-#                 x = 0
-#             end
-#             puts "Vous descendez d'une case, vous êtes sur la case #{x}" 
+    x = 0 
+    nb_lances = 0
+    until x == 10
+        print "Le lancé de dé vous donne : "
+        nb = rand(1..6).to_i
+        puts nb
+        nb_lances += 1
+        if nb == 1
+            x -= 1
+            if x < 0
+                x = 0
+            end
+            puts "Vous descendez d'une case, vous êtes sur la case #{x}" 
 
-#         elsif nb ==  5 || nb == 6
-#             x += 1
-#             puts "Vous avancez d'une marche, vous êtes sur la case #{x} "
+        elsif nb ==  5 || nb == 6
+            x += 1
+            puts "Vous avancez d'une marche, vous êtes sur la case #{x} "
 
-#         else
-#             puts "Rien ne se passe, vous êtes toujours sur la case #{x}"
-#         end
-#     end 
-#     puts "salut tu à atteint la marche numéro #{x}" 
-#     puts ""
-#     puts "FELICITATIONS - Tu as fini le jeu"
+        else
+            puts "Rien ne se passe, vous êtes toujours sur la case #{x}"
+        end
+    end 
+    puts "salut tu à atteint la marche numéro #{x}" 
     
+    puts ""
+    puts "FELICITATIONS - Tu as fini le jeu"
+end
 
-# end
 
-
-
- 
-# ----- 100x ----- 
 def average_finish_time
 
     nb_lances = 0   
@@ -41,44 +37,48 @@ def average_finish_time
         x = 0 
 
         until x == 10
-            puts "Le lancé de dé vous donne"
+            # print "Le lancé de dé vous donne : "
             nb = rand(1..6).to_i
+            # puts nb
             nb_lances += 1
             if nb == 1
                 x -= 1
                 if x < 0
                     x = 0
                 end
-                puts "Vous descendez d'une case, vous êtes sur la case #{x}" 
+                # puts "Vous descendez d'une case, vous êtes sur la case #{x}" 
 
             elsif nb ==  5 || nb == 6
                 x += 1
-                puts "Vous avancez d'une marche, vous êtes sur la case #{x} "
+                # puts "Vous avancez d'une marche, vous êtes sur la case #{x} "
 
             else
-                puts "Rien ne se passe, vous êtes toujours sur la case #{x}"
+                # puts "Rien ne se passe, vous êtes toujours sur la case #{x}"
             end
         end 
-        puts "salut tu à atteint la marche numéro #{x}" 
         puts ""
-        puts "FELICITATIONS - Tu as fini le jeu"
+        puts "Tu as atteint la marche numéro #{x} - FELICITATIONS "
         
     end
-
+    puts ""
+    puts "Tu viens de jouer 100 parties"
+    puts ""
     moy_nb_lances = nb_lances / 100
-    puts moy_nb_lances
+    
+
+    puts "Le nombre de tours moyen pour arriver au 10ème étage après 100 parties est de #{moy_nb_lances}."
+    puts ""
 end
 
 
 
 # ----- PERFORM -----     
 def perform
+    partie
     average_finish_time
-    x = average_finish_time
+    
 end
 
 
 
 perform
-
-
